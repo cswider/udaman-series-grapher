96260b67ed4fca15bd117afed47747c6c404f1e6
Intern <intern@UHERO-EGGS-2.local>
2013-07-22 12:19:10 -1000

    Edited the javascript

--- public/buildin.js
+++ public/buildin.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
 	$("body").css("display", "none");
 	// $(".buildMeIn").hide(0);
-	$("body").fadeIn(1000);
+	$("body").fadeIn(500);
 	
 	$("a.transition").click(function(event){
 		        event.preventDefault();
 		        linkLocation = this.href;
-		        $("body").fadeOut(1000, redirectPage);      
-		});
+		        $("body").fadeOut(500, redirectPage());      
+	});
 	
 		function redirectPage() {
 	    	window.location = linkLocation;
+		}
+		
+		function redirectPage() {
+			if (location.href.indexOf('reload')==-1) location.replace(location.href+'?reload');
+			window.location = linkLocation;
 		}
 });
