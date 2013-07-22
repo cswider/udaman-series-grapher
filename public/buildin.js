ce9963e667dfbb962381ee675d74c71e33ae5106
Intern <intern@UHERO-EGGS-2.local>
2013-07-22 12:09:26 -1000

    Added more fancy Javascript

---
+++ public/buildin.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$("body").css("display", "none");
+	// $(".buildMeIn").hide(0);
+	$("body").fadeIn(1000);
+	
+	$("a.transition").click(function(event){
+		        event.preventDefault();
+		        linkLocation = this.href;
+		        $("body").fadeOut(1000, redirectPage);      
+		});
+	
+		function redirectPage() {
+	    	window.location = linkLocation;
+		}
+});
