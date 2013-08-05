d150cd4e34f8a017ebc82f49a9655ff46b551e4f
Christopher Swider <christopher.swider@gmail.com>
2013-08-05 12:04:09 -1000

    Removed confidential.rb

--- confidential.rb
+++
@@ -1,3 +0,0 @@
-DATABASE_INFORMATION = "postgres://esoihoycwhxzma:SMmGZV-7ZLpokE8pncJa7-aud1@ec2-54-225-68-241.compute-1.amazonaws.com/d62n1cs47o6lpt"
-USER_EMAIL = "cdouglas14@punahou.edu"
-USER_PASSWORD = "ud@m@n "
